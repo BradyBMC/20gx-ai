7b8320570a22a059533f2298b2122fcb928b813e
Brady's Macbook <bradychan01@gmail.com>
2022-07-29 16:11:59 -0700

    finish data parse no test

--- dataset_tool.py
+++ dataset_tool.py
@@ -19,6 +19,28 @@ def has_character(
             return i
     return -1
 
+#----------------------------------------------------------------------------
+
+'''
+Copies ingame character data and returns as list
+May need to restructure how data is saved
+'''
+def gamedata(player: melee.GameState.players) -> list:
+    position = player.position
+    velocity = [
+        player.speed_air_x_self,
+        player.speed_ground_x_self,
+        player.speed_x_attack,
+        player.speed_y_attack,
+        player.speed_y_self
+    ]
+    return [position, velocity, player.facing]
+
+#----------------------------------------------------------------------------
+
+'''
+Process game data and controller inputs from slp -> pkl
+'''
 def convert_dataset(
                     agent: melee.Character=melee.Character.CPTFALCON,
                     adversary: melee.Character=melee.Character.FOX,
@@ -35,7 +57,7 @@ def convert_dataset(
             print('ERROR character not found in slp file')
             return
 
-    f = open('data.pkl', 'w')
+    f = open('data.pkl', 'wb')
 
     data = []
     while True:
@@ -43,42 +65,33 @@ def convert_dataset(
         # step() returns None when the file ends
         if gamestate is None:
             break
+
+        # Each frame has game data and controller data
         frame = {}
-        # Each frame has character data and controller data
-        break
+
+        # Visible game data for 1 frame
+        agent_data = gamedata(gamestate.players[agent_port])
+        adversary_data = gamedata(gamestate.players[adversary_port])
+        frame['Gamedata'] = [agent_data, adversary_data]
+
+        # All controller data for 1 frame
+        controller = gamestate.players[agent_port].controller_state
+        pressed = []
+        for button in controller.button:
+            if controller.button[button] is True:
+                pressed.append(button)
+        control_stick = (round(controller.main_stick[0], 2), round(controller.main_stick[1], 2))
+        c_stick = (round(controller.c_stick[0], 2), round(controller.c_stick[1], 2))
+        inputs = [control_stick, c_stick, pressed]
+        frame['Controller'] = inputs
+        # Missing L/R half press for light shield. Dpad missing bc Luigi and Samus = Cringe
+
+        data.append(frame)
 
     pickle.dump(data, f)
     f.close()
 
-'''
-count = 0
-stick_pos = {}
-while True:
-    gamestate = console.step()
-    # step() returns None when the file ends
-    if gamestate is None:
-        break
-    # print(gamestate.players[1].controller_state)
-    temp = gamestate.players[1].controller_state
-    player = gamestate.players[1]
-    print(player)
-    pressed = []
-    for x in temp.button:
-        if temp.button[x] is True:
-            pressed.append(x)
-    if len(pressed) >= 1:
-        print(gamestate.players[1].position.x, gamestate.players[1].position.y, pressed)
-    # print(type(temp.main_stick))
-    r = (round(temp.main_stick[0], 2), round(temp.main_stick[1], 2))
-    if r not in stick_pos:
-        stick_pos[r] = 1
-    else:
-        stick_pos[r] += 1
-    count += 1
-    break
-print(stick_pos)
-print(count)
-'''
+#----------------------------------------------------------------------------
 
 if __name__ == '__main__':
     convert_dataset()
